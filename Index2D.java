54a8a4c1671af97eb376d9cac97f7a0d1207a300
Lior Lyubarsky <lior.lyubarsky@gmail.com>
2025-12-19 11:37:16 +0200

    Index2D

--- Index2D.java
+++ Index2D.java
@@ -1,5 +1,7 @@
 package assignments.Ex2;
 
+import java.util.Objects;
+
 public class Index2D implements Pixel2D {
 
     private int _x;
@@ -28,32 +30,45 @@ public class Index2D implements Pixel2D {
     @Override
     public int getX() {
 
-        return 0;
+        return this._x;
     }
 
     @Override
     public int getY() {
 
-        return 0;
+        return this._y;
     }
 
     @Override
     public double distance2D(Pixel2D p2) {
+        if (Objects.isNull(p2)) {
+            throw new RuntimeException("p2 is null");
+        }
 
-        return 0;
+        double dx = p2.getX() - this._x;
+        double dy = p2.getY() - this._y;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     @Override
     public String toString() {
         String ans = null;
-
+        ans =   "(" + this._x + ", " + this._y + ")";
         return ans;
     }
 
     @Override
     public boolean equals(Object p) {
+        if(!(p instanceof Index2D)) {
+            return false;
+        }
+        Index2D other = (Index2D) p;
         boolean ans = true;
-
+        if(this._x != other.getX()){
+            ans = false;
+        } else if (this._y != other.getY()) {
+            ans = false;
+        }
         return ans;
     }
 }
